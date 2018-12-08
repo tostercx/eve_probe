148a2d94f1aad4a95d4a0f8f27dae32e597db082
toster <tostercx@gmail.com>
2018-12-09 00:39:02 +0200

    upgrade advapi build script to VS17

--- advapi32/build_vc.bat
+++ advapi32/build_vc.bat
@@ -3,6 +3,7 @@ del advapi32.exp
 del advapi32.lib
 del advapi32.obj
 del advapi32.dll
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" x86
 cl /O2 advapi32.cpp /LD /Feadvapi32.dll /link /DEF:advapi32.def
