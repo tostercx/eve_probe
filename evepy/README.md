6dff77f356224aab9770729a200c055695fe863a
toster <tostercx@gmail.com>
2018-12-15 16:29:34 +0200

    move evepy to a seperate repo

--- evepy/README.md
+++
@@ -1,5 +0,0 @@
-# evepy
-
-Standalone eve python launcher
-
-You might have to mess with getEvePath if not on steam / win x64... or just hardcode it.
--- evepy/build_vc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@del evepy.exe
-@del evepy.obj
-@call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
-@call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
-cl /O2 evepy.cpp
